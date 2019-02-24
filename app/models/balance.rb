bdb308f74dbf9c0c1337834bdb7a63103dda76f9
SPRiiiNG <em@gettgo.com>
2019-02-24 18:41:06 +0700

    fixed law of demeter

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -3,9 +3,7 @@ class Balance < ApplicationRecord
   belongs_to :user
   has_many :transaction_transfers
 
-  def asset_name
-    self.asset.name
-  end
+  delegate :name, :to => :asset, :prefix => true
 
   def amount
     add = self.transaction_transfers.where(asset_type: asset_name, transfer_type: 'add').sum(:amount)
